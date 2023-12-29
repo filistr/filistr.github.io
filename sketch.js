007ffa27988444c0a68c4d190482b017c370aa46
Filippa Strandlund <Filippa@Filippas-MacBook-Air-9.local>
2023-12-29 16:14:48 +0100

    video ftd + octopus h1

--- sketch.js
+++ sketch.js
@@ -22,5 +22,21 @@ window.addEventListener('scroll', () => {
     document.body.style.setProperty('--scroll', window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
   }, false);
 
+  const ftdVideo = document.getElementById("ftd-video");
+let videoPlayed = false;
+
+window.addEventListener("scroll", () => {
+    const rect = ftdVideo.getBoundingClientRect();
+    const windowHeight = window.innerHeight || document.documentElement.clientHeight;
+
+    // Check if the video is in the viewport and hasn't been played yet
+    if (rect.top <= windowHeight && !videoPlayed) {
+        ftdVideo.play();
+        videoPlayed = true;
+    } else if (rect.top > windowHeight) {
+        // Pause the video if it goes out of view
+        ftdVideo.pause();
+    }
+});
 
 

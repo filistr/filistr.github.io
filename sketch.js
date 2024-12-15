ec0e7d09ea41f12c3d6faba49d15f3ea7a2b6fd1
filistr <filippa.strandlund@gmail.com>
2024-12-15 17:18:32 +0100

    animation based in sthlm

--- sketch.js
+++ sketch.js
@@ -107,3 +107,21 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     });
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+    const words = document.querySelectorAll("#basedinsthlm .word");
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("visible");
+            } else {
+                entry.target.classList.remove("visible");
+            }
+        });
+    }, { threshold: 0.5 });
+
+    words.forEach(word => {
+        observer.observe(word);
+    });
+});

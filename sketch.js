b41e756e6a06c3b9a4eca32dea89a17bb2ed8ab5
filistr <filippa.strandlund@gmail.com>
2024-12-15 16:47:31 +0100

    fade in scroll

--- sketch.js
+++ sketch.js
@@ -49,3 +49,20 @@ window.addEventListener("scroll", () => {
         scrollTopButton.style.display = "none";
     }
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+    const sections = document.querySelectorAll('.fade-in');
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+            }
+        });
+    });
+
+    sections.forEach(section => {
+        observer.observe(section);
+    });
+});
+

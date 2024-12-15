c0e3b04713afb32da617f4910bcd0a07629bed6c
filistr <filippa.strandlund@gmail.com>
2024-12-15 16:19:42 +0100

    scroll to top button + formatting

--- sketch.js
+++ sketch.js
@@ -1,26 +1,26 @@
-function scrollToHome() { 
-    var elem = document.getElementById("home"); 
-    elem.scrollIntoView({behavior: 'smooth'}); 
-} 
+function scrollToHome() {
+    var elem = document.getElementById("home");
+    elem.scrollIntoView({ behavior: 'smooth' });
+}
 
-function scrollToProjects() { 
-    var elem = document.getElementById("projects"); 
-    elem.scrollIntoView({behavior: 'smooth'}); 
-} 
+function scrollToProjects() {
+    var elem = document.getElementById("projects");
+    elem.scrollIntoView({ behavior: 'smooth' });
+}
 
-function scrollToAboutMe() { 
-    var elem = document.getElementById("aboutme"); 
-    elem.scrollIntoView({behavior: 'smooth'}); 
-} 
+function scrollToAboutMe() {
+    var elem = document.getElementById("aboutme");
+    elem.scrollIntoView({ behavior: 'smooth' });
+}
 
-function scrollToContact() { 
-    var elem = document.getElementById("contact"); 
-    elem.scrollIntoView({behavior: 'smooth'}); 
-} 
+function scrollToContact() {
+    var elem = document.getElementById("contact");
+    elem.scrollIntoView({ behavior: 'smooth' });
+}
 
 window.addEventListener('scroll', () => {
     document.body.style.setProperty('--scroll', window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
-  }, false);
+}, false);
 
 const ftdVideo = document.getElementById("ftd-video");
 let videoPlayed = false;
@@ -37,4 +37,15 @@ window.addEventListener("scroll", () => {
     }
 });
 
+function scrollToTop() {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+}
 
+const scrollTopButton = document.getElementById("scroll-top");
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 300) {
+        scrollTopButton.style.display = "block";
+    } else {
+        scrollTopButton.style.display = "none";
+    }
+});
